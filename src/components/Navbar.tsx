3c206b4f31461a476217e2a709d46d81c4d87974
Denuke <ddnadhananja@gmail.com>
2022-03-20 02:39:13 +0530

    Added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import './Navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+      <div className="navbar-container">
+          <div className='navbar'>
+              <div className='navbar-left'>
+                  EN
+                  <div className="search-container">
+                    <input type='text'/>
+                    <SearchIcon className='search' style={{color: 'grey' , fontSize: 20}}/>
+                  </div>
+              </div>
+              <div className='navbar-middle'>Denuke Palace</div>
+              <div className='navbar-right'>
+                  <div className="menu-item">Login</div>
+                  <div className="menu-item">Join Us</div>
+                  <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlinedIcon color="action" />
+                    </Badge>
+              </div>
+         </div>
+      </div>
+  )
+}
+
+export default Navbar 
