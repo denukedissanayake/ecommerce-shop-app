60ab9c5b50ec96eb2f1066084370bc7c88e32005
Denuke <ddnadhananja@gmail.com>
2022-03-20 18:44:47 +0530

    Added hotnews bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Hotnews from './components/Hotnews';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Hotnews/>
       <Navbar/>
       <Home/>
     </div>
---
+++ src/components/Hotnews.tsx
@@ -0,0 +1,11 @@
+import './Hotnews.css'
+
+type Props = {}
+
+const Hotnews = (props: Props) => {
+  return (
+    <div className='hot-news'>Lady Gaga Pulls Off Two Red Carpets In One Night</div>
+  )
+}
+
+export default Hotnews
