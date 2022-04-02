3675b28f77854e72cb5f5d0a1fc01c005ac8b46a
Denuke <ddnadhananja@gmail.com>
2022-04-03 00:55:00 +0530

    Add Display Shopping Cart - Summary Purchaged Items

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,6 @@
 import Add from '@mui/icons-material/Add'
 import Remove from '@mui/icons-material/Remove'
+import { fontSize } from '@mui/system'
 import './styles/Cart.css'
 
 type CartProductProps = {
@@ -37,6 +38,15 @@ const Cart = () => {
         )
     }
 
+    const SummaryItem = ({topic, price, isTotal} : {topic: string, price: string, isTotal?: boolean} ) => {
+        return (
+            <div className='summary-item'>
+                <p style={isTotal ? { fontWeight: "bold" ,fontSize: "25px" }: {}} className='summary-item-title'>{topic}</p>
+                <p style={isTotal ? { fontWeight: "bold" ,fontSize: "25px" }: {}} className='summary-item-price'>${price}</p>
+            </div>
+        )
+    }
+
   return (
       <div className='cart-container'>
           <h1 className="cart-title">Shopping Items</h1>
@@ -52,40 +62,17 @@ const Cart = () => {
                   <CartProduct image="https://www.tezenis.com/dw/image/v2/BCXQ_PRD/on/demandware.static/-/Sites-TEZ_EC_COM/default/dw52cd15a2/images/2MM15C9120-F.jpg?sw=600&sfrm=jpeg"/>
                   <CartProduct image="https://multimedia.bbycastatic.ca/multimedia/products/500x500/109/10967/10967513.jpg"/>
               </div>
-              <div className='purchased-product-summary'>Summary</div>
+              <div className='purchased-product-summary'>
+                  <h1 className="summary-title">Order Summary</h1>
+                  <SummaryItem topic="Sub Total" price="9.9"/>
+                  <SummaryItem topic="Shipping Cost" price="5" />
+                  <SummaryItem topic="Discount" price="2" />
+                  <SummaryItem isTotal={true} topic="Total" price="12.9" />
+                  <button className='summary-checkout-button'>CHECKOUT</button>
+              </div>
           </div>
       </div>
   )
 }
 
 export default Cart
-
-
-
-    // const CartProductItem = () => {
-    //     return (
-    //         <div className='cart-product-details'>
-    //             <img className='cart-product-details-image' src='https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
-    //             <div className='cart-product-details-info'>
-    //                 <p className='cart-product-details-info-name'><b>Product: </b>Black Nike Shoe</p>
-    //                 <p className='cart-product-details-info-id'><b>Product ID: </b>DGGYTND868998</p>
-    //                 <p className='cart-product-details-info-color'><b>Product Color: </b>Black</p>
-    //             </div>
-    //         </div>
-    //     )
-    // }
-
-    // const CartPriceDetails = () => {
-    //     return (
-    //         <div className='cart-price-details'>
-    //             <div className='cart-price-details-amount'>
-    //                 <Remove className='cart-price-details-amount-icon'/>
-    //                 <span className='cart-price-details-amount-number'>2</span>
-    //                 <Add className='cart-price-details-amount-icon'/>
-    //             </div>
-    //             <div className='cart-price-details-price'>
-    //                 <span>$ 9.99</span>
-    //             </div>
-    //         </div>
-    //     )
-    // }
