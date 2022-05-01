f07dc57038b27238bdd48df0e6fed62543f2bba0
Denuke <ddnadhananja@gmail.com>
2022-05-02 01:31:53 +0530

    Changed Backend to seperate repo

--- Backend/src/routes/Payments.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const paymentController = require('../controllers/Payment-Controller');
-const { VerifyToken } = require('../utils/auth-middlewares');
-
-const router = express.Router()
-
-router.post('/', VerifyToken, paymentController.doPayments);
-
-module.exports = router
